31f1648097efa290e4ece1dcd30f37bacf1a041f
Daria Alekseevna <dasha.gugnina98@gmail.com>
2022-07-17 08:20:23 +0300

    решение 1 и 2 задачи

--- task1 (41)/Program.cs
+++ task1 (41)/Program.cs
@@ -1,7 +1,71 @@
 ﻿/* Задача 41: Пользователь вводит с клавиатуры M чисел. Посчитайте, сколько чисел больше 0 ввёл пользователь.
 
 0, 7, 8, -2, -2 -> 2
-1, -7, 567, 89, 223-> 3
- */
+1, -7, 567, 89, 223-> 3 */
 
- 
+
+void FillArray(int[] array)
+{
+    int length = array.Length;
+    int index = 0;
+    while (index < length)
+    {
+        Console.Write("введите элемент массива:");
+         array[index] =  int.Parse(Console.ReadLine());
+        index++;
+    }
+}
+
+void PrintArray(int[] array)
+{
+    int count = array.Length;
+    int position = 0;
+    while (position < count)
+     {
+        if (position == 0 && (count - 1) == 0)
+        {
+            Console.WriteLine("[" + array[position] + "]");
+        }
+        else if (position == 0)
+        {
+            Console.Write("[" + array[position] + ", ");
+        }
+        else if (position == (count - 1))
+        {
+            Console.WriteLine(array[position] + "]");
+        }
+        else
+        {
+            Console.Write(array[position] + ", ");
+        }
+        position++;
+    }
+}
+
+
+
+int M;
+Console.WriteLine("Введите количество чисел:");
+while (!int.TryParse(Console.ReadLine(), out M))
+{
+    System.Console.WriteLine("Введите корректное значение:");
+}
+
+int[] array = new int [M];
+
+FillArray(array);
+Console.WriteLine("массив заданный пользователем:");
+PrintArray(array);
+
+int sum = 0;
+int i = 0;
+while (i < M)
+{
+    if (array[i] > 0)
+    {
+        sum++;
+    }
+    i++;
+}
+
+System.Console.WriteLine(" чисел больльше 0: " + sum);
--- task2 (43)/Program.cs
+++ task2 (43)/Program.cs
@@ -2,3 +2,27 @@
 заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 задаются пользователем.
 
 b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5) */
+
+int b1;
+int k1;
+int b2;
+int k2;
+Console.WriteLine("Введите значения b1, k1, b2 и k2:");
+while ((!int.TryParse(Console.ReadLine(), out b1)) || (!int.TryParse(Console.ReadLine(), out k1)) || (!int.TryParse(Console.ReadLine(), out b2)) || (!int.TryParse(Console.ReadLine(), out k2)))
+{
+    System.Console.WriteLine("Введите корректные значения b1, k1, b2 и k2 заново:");
+}
+
+System.Console.WriteLine("прямые: y = " + k1 + " * x + " + b1+ ", y = "+ k2 + " * x + " + b2);
+
+double x = 0;
+double y = 0;
+double b = 0;
+double k = 0;
+
+b = b2 - b1;
+k = k1 - k2;
+x = b/k;
+y = k1 * x + b1;
+
+System.Console.WriteLine("точка пересечения прямых равна: (" + x + "; " + y + ")");
